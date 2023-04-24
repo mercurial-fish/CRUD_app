71d4c2fec8ca2912b1d50c1667d9c1de71295052
Dezi <dezi.a.cruz20@gmail.com>
2023-04-24 16:31:16 -0500

    final project

--- index.js
+++ index.js
@@ -7,7 +7,10 @@ Use Bootstrap and CSS to style your project.*/
 
 const BOOKS_URL = "https://6441e96533997d3ef904aac0.mockapi.io/CRUD_App_API/books";
 
-$.get(BOOKS_URL).then(data =>
+/*NOTE - retrieves the data from the API, then goes through each object and adds it to the table body and adds a delete button
+that, when clicked will delete the piece of data from the API itself. The function it calls is further below */
+
+    $.get(BOOKS_URL).then(data =>
     data.map(book => {
         $("tbody").append(
             $(`
@@ -16,11 +19,15 @@ $.get(BOOKS_URL).then(data =>
                 <td>${book.title}</td>
                 <td>${book.author}</td>
                 <td>${book.genre}</td>
-                <td><button onclick="deleteBook(${book.id})">Delete</button></td>
+                <td><button class="btn btn-danger" onclick="deleteBook(${book.id})">Delete</button></td>
             </tr>
             `)
         )
     }))
+
+
+/*NOTE - calls on the id for the Submit button in the HTML. Then it sets up a function to be activated when the button is clicked/
+When the button is clicked, it will add the value of the input that has been put in fields called below */
 
     $("#addBook").on("click", () => {
         $.post(BOOKS_URL, {
@@ -30,15 +37,18 @@ $.get(BOOKS_URL).then(data =>
         })
     });
 
-    //FIXME - deletes the book, but it doesn't update until you refresh the page
+    /*NOTE - this function takes an id as its argument and uses that id to target the specific data it wants deleted from the API */
+
     function deleteBook(id) {
         $.ajax(`${BOOKS_URL}/${id}` , {
             method: "DELETE",
         });
     };
 
-    function updateList() {
-
+    /*NOTE - this function fetches the specific information based on the id number given and then allows us to enter in
+    new data for the id and update the API. Prevent default is used to stop the page refreshing before the information is submitted */
+    function updateList(e) {
+        e.preventDefault();
         let id = $("#updateID").val();
       
         fetch(`${BOOKS_URL}/${id}`, {
@@ -53,6 +63,7 @@ $.get(BOOKS_URL).then(data =>
           })
         })
       };
+
       
       
-      $("#updateBook").on("click", updateList);
+      $("#updateBook").on("click", (e) => updateList(e))
